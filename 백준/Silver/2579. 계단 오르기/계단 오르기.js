54302aba790a97814b3777a3463d92510dd616e2
박상훈 <76721795+bigyou98@users.noreply.github.com>
2023-01-14 16:11:08 +0900

    [Silver III] Title: 계단 오르기, Time: 136 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.js
@@ -0,0 +1,19 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
+let [n, ...arr] = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+
+n = parseInt(n);
+
+arr = arr.map(Number);
+const dp = Array(n).fill(0);
+
+dp[0] = arr[0];
+dp[1] = Math.max(arr[0] + arr[1], arr[1]);
+dp[2] = Math.max(arr[0] + arr[2], arr[1] + arr[2]);
+for (let i = 3; i < n; i++) {
+  dp[i] = Math.max(arr[i] + arr[i - 1] + dp[i - 3], arr[i] + dp[i - 2]);
+}
+console.log(dp[n - 1]);
