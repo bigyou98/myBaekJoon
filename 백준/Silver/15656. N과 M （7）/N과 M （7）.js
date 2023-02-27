532567043b69f6ea5836993d86eab605c1f6e714
박상훈 <76721795+bigyou98@users.noreply.github.com>
2023-02-27 15:22:51 +0900

    [Silver III] Title: N과 M (7), Time: 1000 ms, Memory: 175424 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.js
@@ -0,0 +1,29 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
+let [input, arr] = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+const [n, m] = input.split(" ").map(Number);
+arr = arr
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+
+let result = "";
+const temp = [];
+
+const dfs = () => {
+  if (temp.length === m) {
+    result += `${temp.join(" ")}\n`;
+    return;
+  }
+  for (let i = 0; i < n; i++) {
+    temp.push(arr[i]);
+    dfs();
+    temp.pop();
+  }
+};
+
+dfs();
+console.log(result);
