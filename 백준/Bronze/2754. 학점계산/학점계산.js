cbdddd9b9dff3d626c3d2b491b8338296d02653b
박상훈 <76721795+bigyou98@users.noreply.github.com>
2023-03-02 22:05:36 +0900

    [Bronze V] Title: 학점계산, Time: 120 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.js
@@ -0,0 +1,24 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "예제.txt";
+let [input] = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+
+let obj = {
+  "A+": 4.3,
+  A0: (4.0).toFixed(1),
+  "A-": 3.7,
+  "B+": 3.3,
+  B0: (3.0).toFixed(1),
+  "B-": 2.7,
+  "C+": 2.3,
+  C0: (2.0).toFixed(1),
+  "C-": 1.7,
+  "D+": 1.3,
+  D0: (1.0).toFixed(1),
+  "D-": 0.7,
+  F: (0.0).toFixed(1),
+};
+
+console.log(obj[input]);
