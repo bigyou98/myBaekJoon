a1c839c8961b352bf0f51b6afa1ab2eb919b5063
박상훈 <76721795+bigyou98@users.noreply.github.com>
2023-03-22 16:29:49 +0900

    [Gold IV] Title: 연구소, Time: 696 ms, Memory: 15168 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.js
@@ -0,0 +1,76 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '예제.txt';
+let [[n, m], ...map] = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((data) => data.split(' ').map(Number));
+
+const visited = map.map((data) => [...data]);
+const queue = [];
+
+const virus = [];
+// 2일때만 확인
+// 상하좌우 확인하며 0이라면 queue에 넣기
+const dx = [0, 0, -1, 1];
+const dy = [-1, 1, 0, 0];
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < m; j++) {
+    if (visited[i][j] === 2) {
+      virus.push([i, j]);
+    }
+    if (visited[i][j] === 0) {
+      queue.push([i, j]);
+    }
+  }
+}
+const BFS = (q, w, e, r, t, y) => {
+  // 복사한 값
+  const copy_map = map.map((data) => [...data]);
+  copy_map[q][w] = 1;
+  copy_map[e][r] = 1;
+  copy_map[t][y] = 1;
+  let count = 0;
+
+  const virusQueue = virus.map((data) => [...data]);
+
+  while (virusQueue.length) {
+    const [x, y] = virusQueue.shift();
+    for (let i = 0; i < 4; i++) {
+      let nx = x + dx[i];
+      let ny = y + dy[i];
+
+      if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+        continue;
+      }
+      if (copy_map[nx][ny] === 0) {
+        virusQueue.push([nx, ny]);
+        copy_map[nx][ny] = 2;
+      }
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (copy_map[i][j] === 0) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+let answer = 0;
+
+for (let i = 0; i < queue.length; i++) {
+  const [x1, y1] = queue[i];
+  for (let j = i + 1; j < queue.length; j++) {
+    const [x2, y2] = queue[j];
+    for (let k = j + 1; k < queue.length; k++) {
+      const [x3, y3] = queue[k];
+      answer = Math.max(BFS(x1, y1, x2, y2, x3, y3), answer);
+    }
+  }
+}
+
+console.log(answer);
